a71e90eed3ea07d2230d1a41807698c4aea0b930
fenixblanco18 <montero.helena9@gmail.com>
2024-03-09 19:06:08 +0100

    aaa

---
+++ Crazy_chicken/Assets/Animals_FREE/Scripts/chicken.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chicken : MonoBehaviour
+{
+    
+    public float fuerza = 50;
+    
+    private Rigidbody rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonUp("fire1")){
+
+            rb.AddForce(new Vector3(0, fuerza, 0));
+
+        }
+
+    }
+}
